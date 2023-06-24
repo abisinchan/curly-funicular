2d0e54056391469b1bb036fa4d1664c45de76e43
AE SChan <abigailsinchan@gmail.com>
2023-06-23 21:31:59 -0400

    trial, testing links for read me.

--- main/README.md
+++ main/README.md
@@ -1,36 +1,37 @@
-# This is a story about a girl 
-
-## Description
-Her name was lucky
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-Early morning
-
-## Usage
-she wakes up
-
-## Credits
-with a knock knock knock on her dooor
-
-## License
-none
-
-## Contributing
-its take for makeup
-
-## Tests
-perfect smile
-
-## Questions
-For any questions or concerns, please contact:
-- GitHub: [and shes who theyre all](https://github.com/and shes who theyre all)
-- Email: [waiting for](mailto:waiting for)
+# a
+  
+  ## Description
+  b
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  c
+  
+  ## Usage
+  d
+  
+  ## Credits
+  e
+  
+  ## License
+  none
+  
+  ## Contributing
+  g
+  
+  ## Tests
+  j
+  
+  ## Questions
+  For any questions or concerns, please contact:
+  - GitHub: [o](https://github.com/o)
+  - Email: [l](mailto:l)
+  
--- main/index.js
+++ main/index.js
@@ -2,52 +2,53 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 function generateREADME(answers) {
-  const readmeTemplate = `# ${answers.title}
-
-## Description
-${answers.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-${answers.installation}
-
-## Usage
-${answers.usage}
-
-## Credits
-${answers.credits}
-
-## License
-${answers.license}
-
-## Contributing
-${answers.contributing}
-
-## Tests
-${answers.tests}
-
-## Questions
-For any questions or concerns, please contact:
-- GitHub: [${answers.username}](https://github.com/${answers.username})
-- Email: [${answers.email}](mailto:${answers.email})
-`;
-
-  fs.writeFile('README.md', readmeTemplate, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('README.md successfully generated!');
-    }
-  });
-}
+    const readmeTemplate = `# ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## Credits
+  ${answers.credits}
+  
+  ## License
+  ${answers.license}
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## Questions
+  For any questions or concerns, please contact:
+  - GitHub: [${answers.username}](https://github.com/${answers.username})
+  - Email: [${answers.email}](mailto:${answers.email})
+  `;
+  
+    fs.writeFile('README.md', readmeTemplate, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md successfully generated!');
+      }
+    });
+  }
+  
 
 function main() {
   inquirer.prompt([
@@ -72,23 +73,22 @@ function main() {
       message: 'Provide usage information:',
     },
     {
-        type: 'input',
-        name: 'credits',
-        message: 'Provide credits:',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Select license?',
-        choices: [
-          "none",
-          "MIT",
-          "GNU GPLv3",
-          "Mozilla Public License 2.0",
-          "Apache License 2.0",
-      
-        ],
-      },
+      type: 'input',
+      name: 'credits',
+      message: 'Provide credits:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Select license:',
+      choices: [
+        "none",
+        "MIT",
+        "GNU GPLv3",
+        "Mozilla Public License 2.0",
+        "Apache License 2.0",
+      ],
+    },
     {
       type: 'input',
       name: 'contributing',
